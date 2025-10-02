344197a3faf97629807ad900b4756bbad1cc636f
Qianfeng Zhang <Qianfeng.Zhang@amd.com>
2025-10-02 14:27:40 +0000

    Use kN0 == 128 for splitkv_smallq setting on gfx950

--- xformers/csrc/attention/hip_fmha/ck_tiled_fmha_fwd_splitkv_smallq_setting.h
+++ xformers/csrc/attention/hip_fmha/ck_tiled_fmha_fwd_splitkv_smallq_setting.h
@@ -118,28 +118,28 @@ struct FmhaFwdSplitKVSmallQShape<256> {
 
 template <>
 struct FmhaFwdSplitKVSmallQBlockTile<32> {
-  using type = ck_tile::sequence<16, 64, 16, 32, 32, 32>;
+  using type = ck_tile::sequence<16, 128, 16, 32, 32, 32>;
   using gemm0_warps = ck_tile::sequence<1, 2, 1>;
   using gemm1_warps = ck_tile::sequence<1, 2, 1>;
 };
 
 template <>
 struct FmhaFwdSplitKVSmallQBlockTile<64> {
-  using type = ck_tile::sequence<16, 64, 32, 64, 32, 64>;
+  using type = ck_tile::sequence<16, 128, 32, 64, 32, 64>;
   using gemm0_warps = ck_tile::sequence<1, 4, 1>;
   using gemm1_warps = ck_tile::sequence<1, 4, 1>;
 };
 
 template <>
 struct FmhaFwdSplitKVSmallQBlockTile<96> {
-  using type = ck_tile::sequence<16, 64, 32, 128, 32, 96>;
+  using type = ck_tile::sequence<16, 128, 32, 128, 32, 96>;
   using gemm0_warps = ck_tile::sequence<1, 4, 1>;
   using gemm1_warps = ck_tile::sequence<1, 4, 1>;
 };
 
 template <>
 struct FmhaFwdSplitKVSmallQBlockTile<128> {
-  using type = ck_tile::sequence<16, 64, 64, 128, 32, 128>;
+  using type = ck_tile::sequence<16, 128, 64, 128, 32, 128>;
   using gemm0_warps = ck_tile::sequence<1, 4, 1>;
   using gemm1_warps = ck_tile::sequence<1, 4, 1>;
 };
